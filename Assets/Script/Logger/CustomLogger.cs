3470500f296398acf0d403fe8264ab6f0ab93c26
TamagnaDavide <supernikx@gmail.com>
2018-02-07 11:46:44 +0100

    Fix bug CustomLogger non trovato

---
+++ Assets/Script/Logger/CustomLogger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public static class CustomLogger{
+
+    public static LoggerType type = LoggerType.ScreenLogger;
+    public static string currentLogString = string.Empty;
+
+    public static void Log(string messaggio, params Object [] args)
+    {
+
+        switch (type)
+        {
+            case LoggerType.UnityLogger:
+            case LoggerType.ScreenLogger:
+                currentLogString = String.Format(messaggio,args) + Environment.NewLine + currentLogString;
+                UnityEngine.Debug.LogFormat(messaggio, args);
+                break;
+            default:
+                UnityEngine.Debug.LogFormat(messaggio, args);
+                break;
+        }
+    }
+
+}
+
+public enum LoggerType
+{
+    UnityLogger,
+    ScreenLogger,
+}
---
+++ Assets/Script/Logger/ScreenLoggerVisualizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScreenLoggerVisualizer : MonoBehaviour {
+
+    public TextMeshProUGUI textArea;
+	
+	// Update is called once per frame
+	void Update () {
+        textArea.text = CustomLogger.currentLogString;
+	}
+}
