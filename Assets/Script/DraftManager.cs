6644bcc81b4f98243b04baf1a8585ad5484367af
Riccardo Mazzocchi <rikuz91@hotmail.com>
2018-02-15 14:27:33 +0100

    Aggiunta script DraftPawn che sostituisce Pawn in fase draft. Scelta delle pedine in scena draft

--- Assets/Script/DraftManager.cs
+++ Assets/Script/DraftManager.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
 
-public class DraftManager : MonoBehaviour {
+public class DraftManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum PlayerDraftTurn { P1_draft, P2_draft };
+    public PlayerDraftTurn currentDraftTurn;
+
+    public List<DraftPawn> pawns;
+    public List<DraftPawn> P1_pawns;
+    public List<DraftPawn> P2_pawns;
+    BoardManager bm;
+
+    public bool hasDrafted;
+
+    public Image[] p1_picks, p2_picks;
+
+    public GameObject draftButton, playButton;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        pawns = FindObjectsOfType<DraftPawn>().ToList();
+        currentDraftTurn = PlayerDraftTurn.P1_draft;
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DraftRandomPattern()
+    {
+        int i = 0;
+        if (!hasDrafted)
+        {
+            foreach (DraftPawn pawn in pawns)
+            {
+                pawns[i].RandomizeColor();
+                i++;
+            }
+            hasDrafted = true;
+            draftButton.SetActive(false);
+        }
+    }
 }
---
+++ Assets/Script/DraftPawn.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraftPawn : MonoBehaviour
+{
+
+
+    public int[] pawnColorIndex;
+    int indexNumber;
+
+    public Color[] pawnColor = new Color[3];
+    Color color;
+
+    DraftManager dm;
+
+
+    static int i, o;
+    // Use this for initialization
+    void Start()
+    {
+        dm = FindObjectOfType<DraftManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RandomizeColor()
+    {
+        indexNumber = pawnColorIndex[(Random.Range(0, pawnColor.Length))];
+        Debug.Log(indexNumber);
+        if (indexNumber == pawnColorIndex[0])
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = pawnColor[0];
+        }
+        else if (indexNumber == pawnColorIndex[1])
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = pawnColor[1];
+        }
+        else if (indexNumber == pawnColorIndex[2])
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = pawnColor[2];
+        }
+        else if (indexNumber == pawnColorIndex[3])
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = pawnColor[3];
+        }
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (dm.hasDrafted)
+        {
+            if (dm.currentDraftTurn == DraftManager.PlayerDraftTurn.P1_draft)
+            {
+                dm.P1_pawns.Add(this);
+                dm.pawns.Remove(this);
+                dm.p1_picks[i].color = this.gameObject.GetComponent<MeshRenderer>().material.color;
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                gameObject.GetComponent<MeshCollider>().enabled = false;
+                i++;
+                dm.currentDraftTurn = DraftManager.PlayerDraftTurn.P2_draft;
+            }
+            else if (dm.currentDraftTurn == DraftManager.PlayerDraftTurn.P2_draft)
+            {
+                dm.P2_pawns.Add(this);
+                dm.pawns.Remove(this);
+                dm.p2_picks[o].color = this.gameObject.GetComponent<MeshRenderer>().material.color;
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                gameObject.GetComponent<MeshCollider>().enabled = false;
+                o++;
+                dm.currentDraftTurn = DraftManager.PlayerDraftTurn.P1_draft;
+            }
+
+            if (dm.pawns.Count == 0)
+            {
+                dm.playButton.SetActive(true);
+            }
+        }
+    }
+}
