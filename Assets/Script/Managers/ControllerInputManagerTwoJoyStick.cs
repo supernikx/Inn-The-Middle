9838757d1b76acf5c1acf3970d417a95c754aa29
DESKTOP-940M2T5\super <supernikx@gmail.com>
2018-06-10 19:46:42 +0200

    Aggiornato pulsante Restart con Menu (pause menu), aggiunta schermata comandi nel pause menu, fixato bug con tutorial nella fase di piazzamento

--- Assets/Script/Managers/ControllerInputManagerTwoJoyStick.cs
+++ Assets/Script/Managers/ControllerInputManagerTwoJoyStick.cs
@@ -690,7 +690,7 @@ public class ControllerInputManagerTwoJoyStick : MonoBehaviour
             }
         }
         else if ((bm.turnManager.CurrentMacroPhase == TurnManager.MacroPhase.game || bm.turnManager.CurrentMacroPhase == TurnManager.MacroPhase.placing || bm.turnManager.CurrentMacroPhase == TurnManager.MacroPhase.draft) &&
-        ((Input.GetKeyDown(joy1Confirm) && bm.turnManager.CurrentPlayerTurn == bm.p1Faction) || ((Input.GetKeyDown(joy2Confirm) && bm.turnManager.CurrentPlayerTurn == bm.p2Faction))) && bm.TutorialInProgress)
+        ((Input.GetKeyDown(joy1Confirm) && bm.turnManager.CurrentPlayerTurn == bm.p1Faction) || ((Input.GetKeyDown(joy2Confirm) && bm.turnManager.CurrentPlayerTurn == bm.p2Faction))) && bm.TutorialInProgress && !bm.pause)
         {
             bm.tutorial.AButtonPressed();
         }
